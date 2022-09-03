41505e88e79e94880939b15be1a9c1a54dc0601a
sieugene <sieugene@Mail.ru>
2022-09-03 22:36:41 +0300

    add : cra project "movies", clear prev projects

---
+++ apps/movies/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "ui";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
